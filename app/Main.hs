cbfb13e80a1e623c85179deea0711949328e728b
Collin Arnett <collin@arnett.it>
2025-09-20 17:40:02 -0400

    Revert "Tempchange"
    This reverts commit 43c63d80f5e81fdc6da49ca0419ff820e0556032.

--- app/Main.hs
+++ app/Main.hs
@@ -30,23 +30,14 @@ main :: IO ()
 main = do
   opts <- getRecord "Tintin, the tutorial website generator"
   case opts of
-    Run outputDirectory verbose True -> do
+    Run outputDirectory verbose shouldUseCabal -> do
       let outputDir = fromMaybe ".stack-work/tintin/rendered/" outputDirectory
       let logger     = if verbose
                        then Logging.stdOut
                        else Logging.mute
       let filesystem = Filesystem.local
       let process    = Process.local
-      runEffects ( runApp True $ OutputDirectory outputDir ) (logger, filesystem, process)
-
-    Run outputDirectory verbose False -> do
-      let outputDir = fromMaybe ".stack-work/tintin/rendered/" outputDirectory
-      let logger     = if verbose
-                       then Logging.stdOut
-                       else Logging.mute
-      let filesystem = Filesystem.local
-      let process    = Process.local
-      runEffects ( runApp False $ OutputDirectory outputDir ) (logger, filesystem, process)
+      runEffects ( runApp shouldUseCabal $ OutputDirectory outputDir ) (logger, filesystem, process)
 
     Publish verbose documentationDirectory -> do
       let outputDir = fromMaybe ".stack-work/tintin/rendered/" documentationDirectory
